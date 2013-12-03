12ac4f1cab3e2c20f08ad018113be1a755dcc766
Linux User <rick@mini.(none)>
2013-12-03 12:40:55 +0000

    allow web app set a random seed

--- random.scm
+++ random.scm
@@ -1,8 +1,11 @@
 
 (define random-number 17)
 
+(define (setrandomnumber n)
+  (set! random-number n))
+
 (define (random) ; return 0.0 >= n < 1.0
-  (set! random-number (remainder (* random-number 4001) 8009))
+  (setrandomnumber (remainder (* random-number 4001) 8009))
   (/ (remainder random-number 1000) 1000.0))
 
 (random)
